36017d38963b7bb495fc4f6776d0bc1d898d8745
HobeXD <b99902113@ntu.edu.tw>
2022-08-01 12:43:31 +0800

    Update attack_upload.js

--- attack_upload.js
+++ attack_upload.js
@@ -1,5 +1,14 @@
 var button = document.createElement("button");
 button.innerHTML = "Upload";
+button.style.position = 'absolute';
+button.style.x = 10;
+button.style.y = 10;
+button.style.width = 300;
+button.style.height = 300;
+button.style.padding = '10px';
+button.style.background = '#00ff00';
+button.style.display = 'block';
+button.style.zIndex = 99;
 
 // 2. Append somewhere
 var body = document.getElementsByTagName("body")[0];
