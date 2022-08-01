6c989bb5086f5ad75d73eaa7bde346f95c7b5fa3
HobeXD <b99902113@ntu.edu.tw>
2022-08-01 12:55:04 +0800

    Update attack_upload.js

--- attack_upload.js
+++ attack_upload.js
@@ -1,12 +1,12 @@
 var button = document.createElement("button");
-button.innerHTML = "Upload";
-button.style.position = 'absolute';
-button.style.x = 10;
-button.style.y = 10;
+button.innerHTML = "複製公波";
+button.style.position = 'fixed';
+button.style.top = "10px";
+button.style.left = "10px";
 button.style.width = 300;
 button.style.height = 300;
 button.style.padding = '10px';
-button.style.background = '#00ff00';
+button.style.background = '#0f0f0f';
 button.style.display = 'block';
 button.style.zIndex = 99;
 
@@ -17,4 +17,5 @@ body.appendChild(button);
 // 3. Add event handler
 button.addEventListener ("click", function() {
     alert("did something");
+    navigator.clipboard.writeText("1,2,3");
 });
