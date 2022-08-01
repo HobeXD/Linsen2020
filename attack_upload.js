efeadef32a7f65a1ab980a40c8bfc2de786b0051
HobeXD <b99902113@ntu.edu.tw>
2022-08-01 13:35:27 +0800

    Update attack_upload.js

--- attack_upload.js
+++ attack_upload.js
@@ -1,12 +1,12 @@
 var button = document.createElement("button");
-button.innerHTML = "複製公波";
+button.innerHTML = "複製攻波";
 button.style.position = 'fixed';
 button.style.top = "10px";
 button.style.left = "10px";
 button.style.width = 300;
 button.style.height = 300;
 button.style.padding = '10px';
-button.style.background = '#0f0f0f';
+button.style.background = '#f0f0f0';
 button.style.display = 'block';
 button.style.zIndex = 99;
 
@@ -16,6 +16,16 @@ body.appendChild(button);
 
 // 3. Add event handler
 button.addEventListener ("click", function() {
-    alert("did something");
     navigator.clipboard.writeText("1,2,3");
+    var copyText = "";
+    var attacks = document.getElementsByClassName("inAttack");
+    for (var i = 0; i < attacks.length; i++) {
+      attacker = attacks[i].getElementsByTagName("thead")[0].getElementsByTagName("td")[1].getElementsByTagName("a")[1].innerHTML.split(" ")[0];
+      attackerPosX = attacks[i].getElementsByClassName("coordinateX")[0].innerHTML.replace(/[^\d.-]/g, '');
+      attackerPosY = attacks[i].getElementsByClassName("coordinateY")[0].innerHTML.replace(/[^\d.-]/g, '');
+      attackTime = attacks[i].getElementsByClassName("at")[0].innerHTML.replace(/[^\d:]/g, '');
+      copyText += attacker + '\t' + attackerPosX + '|' + attackerPosY + '\t' + attackTime + '\n';
+    }
+    navigator.clipboard.writeText(copyText);
+    alert("複製完成");
 });
